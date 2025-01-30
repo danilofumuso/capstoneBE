548d128a811709dfdadb358ea2c5b86edb87f14c
danilofumuso <danilofumuso@gmail.com>
2025-01-30 21:33:11 +0100

    added @Service notation to SectorService

--- src/main/java/it/epicode/capstone/sector/SectorService.java
+++ src/main/java/it/epicode/capstone/sector/SectorService.java
@@ -5,7 +5,9 @@ import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SectorService {
 
     @Autowired
