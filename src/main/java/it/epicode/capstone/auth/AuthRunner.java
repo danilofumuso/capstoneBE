3648b1e62aaa23c6b30351a12d7d5da8969305af
danilofumuso <danilofumuso@gmail.com>
2025-02-24 15:54:00 +0100

    review

--- src/main/java/it/epicode/capstone/auth/AuthRunner.java
+++ src/main/java/it/epicode/capstone/auth/AuthRunner.java
@@ -30,7 +30,7 @@ public class AuthRunner implements ApplicationRunner {
             adminRequest.setEmail("admin@epicode.it");
             adminRequest.setName("Admin");
             adminRequest.setSurname("Admin");
-            adminRequest.setDateOfBirth(LocalDate.of(1998,11,12));
+            adminRequest.setDateOfBirth(LocalDate.of(1998, 11, 12));
             appUserService.registerUser(adminRequest, null, Set.of(Role.ROLE_ADMIN));
         }
 
@@ -43,7 +43,7 @@ public class AuthRunner implements ApplicationRunner {
             studentRequest.setEmail("student@epicode.it");
             studentRequest.setName("Student");
             studentRequest.setSurname("Student");
-            studentRequest.setDateOfBirth(LocalDate.of(1991, 5,21));
+            studentRequest.setDateOfBirth(LocalDate.of(1991, 5, 21));
             appUserService.registerUser(studentRequest, null, Set.of(Role.ROLE_STUDENT));
         }
 
@@ -55,7 +55,7 @@ public class AuthRunner implements ApplicationRunner {
             professionalRequest.setEmail("professional@epicode.it");
             professionalRequest.setName("Professional");
             professionalRequest.setSurname("Professional");
-            professionalRequest.setDateOfBirth(LocalDate.of(1978,3,30));
+            professionalRequest.setDateOfBirth(LocalDate.of(1978, 3, 30));
             appUserService.registerUser(professionalRequest, null, Set.of(Role.ROLE_PROFESSIONAL));
 
             // Professional 1: John Smith
